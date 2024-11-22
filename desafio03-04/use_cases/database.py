bfda80a919585ba3106e5b4dfc33b5463d11b349
Carlos Henrique <carlosgfkp@outook.com>
2024-11-22 20:30:06 -0300

    segundo commit

--- desafio03-04/use_cases/database.py
+++ desafio03-04/use_cases/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Configuração do banco de dados
-DATABASE_URL = "postgresql+psycopg2://postgres:tata1212@localhost:5432/desafio01"
+# Configuração do banco de dados com o nome do serviço do Docker Compose
+DATABASE_URL = "postgresql+psycopg2://postgres:tata1212@db:5432/desafio01"
 engine = create_engine(DATABASE_URL)
 
 # Criar a SessionLocal, que vai ser usada para gerar sessões de banco
